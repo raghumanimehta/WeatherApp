064ed3163783366a3dcd6924346686d5d0d6918f
raghumanimehta <rmehta07@students.cs.ubc.ca>
2024-08-12 17:30:16 -0700

    minor changes to api and script

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 import * as Api from './handle_api.js'
 
 
-const cityInput = document.getElementById('city-input'); // get the value of the input field/trim the value/remove any white spaces/to lower case
-const getWeatherButton = document.getElementById('get-weather-button'); // get the button element
+const cityInput = document.getElementById('city-input'); // get the value of the input field/trim the value/remove any white spaces/to lower cas
 const temperatureDisplay = document.getElementById('temperature-display');
 const feelsLikeDisplay = document.getElementById('feels-like-display');
 const highLowDisplay = document.getElementById('high-low-display');
@@ -10,17 +9,24 @@ const humidityDisplay = document.getElementById('humidity-display');
 const windSpeedDisplay = document.getElementById('wind-speed-display');
 const windDirectionDisplay = document.getElementById('wind-direction-display');
 const windGustDisplay = document.getElementById('wind-gust-display');
-const hourlyForecast = document.getElementById('hourly-forecast');
-const dailyForecast = document.getElementById('daily-forecast');
+const weatehrDescriptionDisplay = document.getElementById('weather-description-display');
+const localTimeDisplay = document.getElementById('local-time-display');
+
+const weatherIconDisplay = document.getElementById('weather-icon-display');
 
 
 
 document.getElementById('search-form').addEventListener('submit', async (event) => {
     event.preventDefault();
     
-    const weatherInfoSection = document.getElementById('weather-info');
     const weatherInfoDisplay = document.getElementById('weather-info-display');
-    weatherInfoSection.style.display = 'block';
+
+
+    await showWeatherData(weatherInfoDisplay);
+    // await showForecastData(weatherInfoSection, weatherInfoDisplay);
+});
+
+async function showWeatherData(weatherInfoDisplay) {
     weatherInfoDisplay.style.display = 'block';
 
     if (cityInput == '') {
@@ -32,7 +38,9 @@ document.getElementById('search-form').addEventListener('submit', async (event) 
                 throw new Error('Invalid city name or country code');
             }
             const weatherData = await Api.getWeatherData(city, country);
-            weatherInfoDisplay.textContent = 'Showing weather results for: ' + cityInput.value;
+            weatherInfoDisplay.textContent = city + ', ' + country;
+            // localTimeDisplay.textContent = 
+            weatehrDescriptionDisplay.textContent = weatherData.weather[0].description.charAt(0).toUpperCase() + weatherData.weather[0].description.slice(1);
             temperatureDisplay.textContent = `Temperature: ${weatherData.main.temp}°C`;
             feelsLikeDisplay.textContent = `Feels like: ${weatherData.main.feels_like}°C`;
             highLowDisplay.textContent = `High: ${weatherData.main.temp_max}°C,  Low: ${weatherData.main.temp_min}°C`;
@@ -40,16 +48,16 @@ document.getElementById('search-form').addEventListener('submit', async (event) 
             windSpeedDisplay.textContent = `Wind Speed: ${weatherData.wind.speed} m/s`;
             windDirectionDisplay.textContent = `Wind Direction: ${weatherData.wind.deg}°`;
             windGustDisplay.textContent = `Wind Gust: ${weatherData.wind.gust ? weatherData.wind.gust + ' m/s' : 'N/A'}`;
-            
-            document.getElementById('current-weather').style.display = 'block';
-            document.getElementById('weather-forecast').style.display = 'block';
+
         } catch (error) {
             weatherInfoDisplay.textContent = 'Error in fetching weather data. Please check the city name and the format and try again';
             console.error('Error in getWeatherData:', error.message);
         }
     }
-});
+}
 
+// async function showForecastData(weatherInfoSection) {
 
+// }
 // Api.getForecastData('Vancouver', 'CA'); // For testing
 // getWeatherData('Mohali', 'IND'); // For testing
